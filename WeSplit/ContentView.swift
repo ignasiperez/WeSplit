940a551b68da1352b380c04dc39389ae3e746680
Ignasi Perez <perezignasi@gmail.com>
2019-10-31 07:41:48 +0100

    Creating pickers in a form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+  
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", text: $checkAmount)
+            .keyboardType(.decimalPad)
+          Picker("Number of people",
+                 selection: $numberOfPeople) {
+                  ForEach(2 ..< 100) {
+                    Text("\($0) people")
+                  }
+          }
+        }
+        Section {
+          Text("$\(checkAmount)")
+        }
+      }
+    .navigationBarTitle("WeSplit")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
